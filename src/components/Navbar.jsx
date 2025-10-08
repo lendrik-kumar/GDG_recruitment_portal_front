21e1465a649988c4b150aaa52c350179e30dd715
lendrik-kumar <sanketkamboj2006@gmail.com>
2025-10-09 01:52:50 +0530

    Revert "fixed reload"
    This reverts commit 8510b7289ee5a8f5ee5a11254e68a2ddec0d4238.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,11 +26,13 @@ const Navbar = () => {
     <motion.header 
       initial={{ y: -100 }}
       animate={{ y: 0 }}
-      className="bg-gray-100 backdrop-blur-sm border-b border-gray-200 px-4 py-4 sticky top-0 z-50">
-      <div className="max-w-8xl mx-auto flex justify-between items-center">
+      className="bg-gray-100 backdrop-blur-sm 
+                 border-b border-gray-200  px-6 py-4 sticky top-0 z-50"
+    >
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
         {/* Left Logo Section */}
         <motion.div 
-          className="flex ml-4 items-center gap-4"
+          className="flex items-center gap-4"
           initial={{ opacity: 0, x: -20 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ delay: 0.2 }}
@@ -48,7 +50,8 @@ const Navbar = () => {
             />
           </motion.div>
           <div>
-            <h1 className="text-xl font-bold text-black bg-clip-text suse-mono">
+            <h1 className="text-xl font-bold bg-gradient-to-r from-blue-600 to-blue-800 
+                          bg-clip-text text-transparent">
               GDG Recruitment Portal
             </h1>
           </div>
@@ -86,7 +89,7 @@ const Navbar = () => {
           </div>
 
           <motion.button
-            className="p-2 text-gray-500 mr-4 hover:text-red-500 rounded-lg 
+            className="p-2 text-gray-500 hover:text-red-500 rounded-lg 
                        hover:bg-red-50 transition-colors"
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
