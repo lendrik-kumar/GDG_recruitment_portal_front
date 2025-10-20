a12a1074e9932ec5361dcdfa6cb53a75cf734a11
Sanket <sanketkamboj2006@gmail.com>
2025-10-20 14:00:07 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const apiClient = axios.create({
-    baseURL: `${import.meta.env.VITE_API_KEY}/api`,
+    baseURL: `${import.meta.env.VITE_API_KEY}api`,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
