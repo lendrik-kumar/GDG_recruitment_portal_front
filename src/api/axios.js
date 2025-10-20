3109cfa8a5c33115e2dbf3fa36325f38154189e3
lendrik-kumar <sanketkamboj2006@gmail.com>
2025-10-20 13:56:16 +0530

    url updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000";
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: `${import.meta.env.VITE_API_KEY}/api`,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
